6ed7d948fcc9d4c337a35aca351ca16d916a982d
jerojiang(江源) <jerojiang@tencent.com>
2016-12-13 22:04:14 +0800

    feat: add log.js

---
+++ app/lib/log.js
@@ -0,0 +1,14 @@
+/**
+ * log 代替 console.log
+ * @author jero
+ * @date 2016-12-13
+ */
+
+'use strict';
+const logger = require('tracer').colorConsole({
+  format: '{{file}}:{{line}} {{message}}',
+});
+
+logger.log('hello');
+logger.trace('hello');
+logger.debug('hello');
